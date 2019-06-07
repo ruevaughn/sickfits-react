bf7b654d8c1182617cbd2587402ce39c2084710a
Chase Jensen <soccerchase@gmail.com>
2019-06-07 16:42:39 -0600

    Replace delete button with button component on Item component

--- components/Item.js
+++ components/Item.js
@@ -6,6 +6,7 @@ import ItemStyles from "./styles/ItemStyles";
 import PriceTag from "./styles/PriceTag";
 import Link from "next/link";
 import formatMoney from "../lib/formatMoney";
+import DeleteItem from "./DeleteItem";
 
 class Item extends Component {
   static propTypes = {
@@ -38,7 +39,7 @@ class Item extends Component {
             <a>Edit ✏️</a>
           </Link>
           <button>Add To Cart</button>
-          <button>Delete</button>
+          <DeleteItem id={item.id}>Delete Item</DeleteItem>
         </div>
       </ItemStyles>
     );
