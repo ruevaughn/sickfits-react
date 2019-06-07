ee0e5f478fa65595e461572417e9138c38827a38
Chase Jensen <soccerchase@gmail.com>
2019-06-07 13:59:59 -0600

    Item add delete button

--- components/Item.js
+++ components/Item.js
@@ -6,41 +6,61 @@ import ItemStyles from "./styles/ItemStyles";
 import PriceTag from "./styles/PriceTag";
 import Link from "next/link";
 import formatMoney from "../lib/formatMoney";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+
+const DELETE_ITEM_MUTATION = gql`
+  mutation DELETE_ITEM_MUTATION($id: ID!) {
+    deleteItem(where: { id: $id }) {
+      id
+    }
+  }
+`;
 
 class Item extends Component {
   static propTypes = {
     item: PropTypes.object.isRequired
   };
+
+  deleteItem = async deleteItemMutation => {
+    console.log(this.props.id);
+    return deleteItemMutation({ variables: { id: this.props.id } });
+  };
+
   render() {
     const { item } = this.props;
     return (
-      <ItemStyles>
-        {item.image && <img src={item.image} alt={item.title} />}
-        <Link
-          href={{
-            pathname: "/item",
-            query: { id: item.id }
-          }}
-        >
-          <Title>
-            <a>{item.title}</a>
-          </Title>
-        </Link>
-        <PriceTag>{formatMoney(item.price)}</PriceTag>
-        <p>{item.description}</p>
-        <div className="buttonList">
-          <Link
-            href={{
-              pathname: "update",
-              query: { id: item.id }
-            }}
-          >
-            <a>Edit ✏️</a>
-          </Link>
-          <button>Add To Cart</button>
-          <button>Delete</button>
-        </div>
-      </ItemStyles>
+      <Mutation mutation={DELETE_ITEM_MUTATION} variables={this.props}>
+        {(deleteItem, { error, loading }) => (
+          <ItemStyles>
+            {item.image && <img src={item.image} alt={item.title} />}
+            <Link
+              href={{
+                pathname: "/item",
+                query: { id: item.id }
+              }}
+            >
+              <Title>
+                <a>{item.title}</a>
+              </Title>
+            </Link>
+            <PriceTag>{formatMoney(item.price)}</PriceTag>
+            <p>{item.description}</p>
+            <div className="buttonList">
+              <Link
+                href={{
+                  pathname: "update",
+                  query: { id: item.id }
+                }}
+              >
+                <a>Edit ✏️</a>
+              </Link>
+              <button>Add To Cart</button>
+              <button onClick={this.deleteItem(deleteItem())}>Delete</button>
+            </div>
+          </ItemStyles>
+        )}
+      </Mutation>
     );
   }
 }
