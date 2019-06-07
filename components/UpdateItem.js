366baca509183f8a849620a7626ff89b6ca81acd
Chase Jensen <soccerchase@gmail.com>
2019-06-07 11:51:06 -0600

    Add updateItem Component

---
+++ components/UpdateItem.js
@@ -0,0 +1,121 @@
+import React, { Component } from "react";
+import { Mutation, Query } from "react-apollo";
+import gql from "graphql-tag";
+import Form from "./styles/Form";
+import formatMoney from "../lib/formatMoney";
+import Error from "./ErrorMessage";
+import Router from "next/router";
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    item(where: { id: $id }) {
+      id
+      title
+      price
+      description
+    }
+  }
+`;
+
+const UPDATE_ITEM_MUTATION = gql`
+  mutation UPDATE_ITEM_MUTATION(
+    $id: ID!
+    $title: String
+    $description: String
+    $price: Int
+  ) {
+    updateItem(id: $id, title: $title, description: $description, price: $price) {
+      id
+      title
+      description
+      price
+    }
+  }
+`;
+
+class UpdateItem extends Component {
+  state = {};
+  // Instance Property - we can access this inside for this.setState
+  // If this was a regular method, es6 classes do not bind regular methods
+  // To the instance of the property
+  updateItem = async (e, updateItemMutation) => {
+    e.preventDefault();
+    const res = await updateItemMutation({
+      variables: {
+        id: this.props.id,
+        ...this.state
+      }
+    });
+    console.log("Updated!!");
+  };
+
+  handleChange = e => {
+    const { name, type, value } = e.target;
+    const val = type === "number" ? parseFloat(value) : value;
+    this.setState({ [name]: val });
+  };
+
+  render() {
+    const { id } = this.props;
+
+    return (
+      <Query query={SINGLE_ITEM_QUERY} variables={{ id }}>
+        {({ data, loading }) => {
+          if (loading) return <p>Loading...</p>;
+          if (!data.item) return <p>No Item found for ID: {this.props.id}</p>;
+          return (
+            <Mutation mutation={UPDATE_ITEM_MUTATION} variables={this.state}>
+              {(updateItem, { loading, error }) => (
+                <Form onSubmit={e => this.updateItem(e, updateItem)}>
+                  <Error error={error} />
+                  <fieldset disabled={loading} aria-busy={loading}>
+                    <label htmlFor="title">
+                      Title
+                      <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        placeholder="Title"
+                        required
+                        defaultValue={data.item.title}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                    <label htmlFor="price">
+                      Price
+                      <input
+                        type="number"
+                        id="price"
+                        name="price"
+                        placeholder="Price"
+                        required
+                        defaultValue={data.item.price}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                    <label htmlFor="description">
+                      Description
+                      <textarea
+                        type="text"
+                        id="description"
+                        name="description"
+                        placeholder="Enter a Description"
+                        required
+                        defaultValue={data.item.description}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                    <button type="submit">Save Changes</button>
+                  </fieldset>
+                </Form>
+              )}
+            </Mutation>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default UpdateItem;
+export { UPDATE_ITEM_MUTATION };
