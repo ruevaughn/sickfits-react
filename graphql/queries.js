be8469d8adc383bfbbb92a9b0f4c3da95f1dca6d
Chase Jensen <soccerchase@gmail.com>
2019-06-17 18:07:27 -0600

    Update gql queries requires

--- graphql/queries.js
+++ graphql/queries.js
@@ -1,5 +1,5 @@
 import gql from "graphql-tag";
-import { perPage } from "../../config";
+import { perPage } from "../config";
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage})  {
